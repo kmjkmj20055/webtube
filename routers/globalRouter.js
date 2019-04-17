6efb2daee577d56fa497a54ee1af70fb2f870b0e
김민정 <kimminjeong@gimminjeongs-MacBook-Pro.local>
2019-04-17 22:53:11 +0900

    빠진 라우터 추가 연결

--- routers/globalRouter.js
+++ routers/globalRouter.js
@@ -1,19 +1,22 @@
 import express from "express";
 import routes from "../routes";
-import { home, search, editVideo } from "../controller/videoController";
+import { home, search, editVideo, upload, videoDetail, deleteVideo } from "../controller/videoController";
 import { join, login, logout, changePassword, editProfile, userDetail } from "../controller/userController";
 
 const globalRouter = express.Router();
 
 globalRouter.get(routes.home, home);
 globalRouter.get(routes.search, search);
+globalRouter.get(routes.upload, upload);
+globalRouter.get(routes.videoDetail, videoDetail);
+globalRouter.get(routes.editVideo, editVideo);
+globalRouter.get(routes.deleteVideo, deleteVideo);
+
 globalRouter.get(routes.join, join);
 globalRouter.get(routes.login, login);
 globalRouter.get(routes.logout, logout);
-
-globalRouter.get(routes.changePassword, changePassword);
-globalRouter.get(routes.editProfile, editProfile);
-globalRouter.get(routes.editVideo, editVideo);
 globalRouter.get(routes.userDetail, userDetail);
+globalRouter.get(routes.editProfile, editProfile);
+globalRouter.get(routes.changePassword, changePassword);
 
 export default globalRouter;
