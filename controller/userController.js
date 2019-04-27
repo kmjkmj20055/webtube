867b6fb058319253e1245af25cdc106808252a91
김민정 <kimminjeong@gimminjeongs-MacBook-Pro.local>
2019-04-27 16:54:22 +0900

    위치 및 라우터 변경
     - 아 노마드코더 진짜 짱나게하네 ㅂㄷㅂㄷ
     - 아직 안맞을 수 도 있음
    
    참고
     - app.js에서 마지막에 app.use()를 확인 ㄲ
       routes를 라우팅해보면 (타고 넘어가보면)
       - home : "/"
       - users : "/users"
       - videos : "/videos"
    
       - 따라서 경로가 /user, /videos로 시작함 ex) /user/:id, /videos/upload

--- controller/userController.js
+++ controller/userController.js
@@ -1,6 +1,7 @@
-export const join = (req, res) => res.render("join");
-export const login = (req, res) => res.render("login");
-export const logout = (req, res) => res.render("logout");
-export const userDetail = (req, res) => res.render("userDetail");
-export const editProfile = (req, res) => res.render("editProfile");
-export const changePassword = (req, res) => res.render("changePassword");
+export const join = (req, res) => res.render("join", {join : "Join"});
+export const login = (req, res) => res.render("login", {login : "Login"});
+//export const login = (req, res) => res.render("login", {potato : 12345});
+export const logout = (req, res) => res.render("logout", {logout : "Logout"});
+export const userDetail = (req, res) => res.render("userDetail", {userDetail : "UserDetail"});
+export const editProfile = (req, res) => res.render("editProfile", {editProfile : "Edit Profile"});
+export const changePassword = (req, res) => res.render("changePassword", {changePassword : "Change Password"});
--- routers/globalRouter.js
+++ routers/globalRouter.js
@@ -1,22 +1,12 @@
 import express from "express";
 import routes from "../routes";
-import { home, search, editVideo, upload, videoDetail, deleteVideo } from "../controller/videoController";
-import { join, login, logout, changePassword, editProfile, userDetail } from "../controller/userController";
+import { home, search, upload } from "../controller/videoController";
+import { join } from "../controller/userController";
 
 const globalRouter = express.Router();
 
+globalRouter.get(routes.join, join);
 globalRouter.get(routes.home, home);
 globalRouter.get(routes.search, search);
-globalRouter.get(routes.upload, upload);
-globalRouter.get(routes.videoDetail, videoDetail);
-globalRouter.get(routes.editVideo, editVideo);
-globalRouter.get(routes.deleteVideo, deleteVideo);
-
-globalRouter.get(routes.join, join);
-globalRouter.get(routes.login, login);
-globalRouter.get(routes.logout, logout);
-globalRouter.get(routes.userDetail, userDetail);
-globalRouter.get(routes.editProfile, editProfile);
-globalRouter.get(routes.changePassword, changePassword);
 
 export default globalRouter;
