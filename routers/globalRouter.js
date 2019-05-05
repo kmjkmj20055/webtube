5f782391288d499d42c61a95980f5ee79fa645c8
김민정 <kimminjeong@gimminjeongs-MacBook-Pro.local>
2019-05-06 00:24:42 +0900

    join, login 페이지 내용 추가
     - 현재 button들은 작동 안함
    
    참고
     - 노마드코더 따라했는데 혼자 중간에 엄청바꿔서 안돼는 url이 있음
       (이번에도 /login 수정함 ㅂㄷ)

--- routers/globalRouter.js
+++ routers/globalRouter.js
@@ -1,11 +1,12 @@
 import express from "express";
 import routes from "../routes";
-import { home, search, upload } from "../controller/videoController";
-import { join } from "../controller/userController";
+import { home, search } from "../controller/videoController";
+import { join, login } from "../controller/userController";
 
 const globalRouter = express.Router();
 
 globalRouter.get(routes.join, join);
+globalRouter.get(routes.login, login);
 globalRouter.get(routes.home, home);
 globalRouter.get(routes.search, search);
 
